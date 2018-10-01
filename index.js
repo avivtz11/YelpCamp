edeeb1fe0754c373379b034d2b87a850c3bde99e
avivtz11 <avivtz1@gmail.com>
2018-10-01 18:39:23 +0300

    fixed port2

--- index.js
+++ index.js
@@ -48,6 +48,6 @@ app.use(indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(process.env.PORT || 3000,process.env.IP || "localhost",function(){
+app.listen(process.env.PORT || "3000",process.env.IP || "localhost",function(){
     console.log("server started");
 });
